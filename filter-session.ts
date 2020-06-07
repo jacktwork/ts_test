355b06e53fba62eadfbe4b3b6b75abb5702b2c95
Jack Tuszynski <31741621+jacktwork@users.noreply.github.com>
2020-06-07 07:03:07 -0700

    Add files via upload

--- filter-session.ts
+++ filter-session.ts
@@ -15,25 +15,27 @@ export class FilterSession implements IFilter {
       return [];
     }
 
-    let foundPhoto = services.find(service => service === "Photography");
-    let foundVideo = services.find(service => service === "VideoRecording");
     const foundSession = services.find(service => service === "WeddingSession");
-    const foundCombo = services.find(service => service === "ComboPhotoVideo");
 
-    if (foundSession && (foundPhoto || foundVideo || foundCombo)) {
-      // turn off session
-      services.map(service => {
-        switch(service) {
-          case 'WeddingSession':
-            services = services.filter(service => service !== 'WeddingSession');
-            break;
+    if (foundSession) {
+      const foundCombo = services.find(service => service === "ComboPhotoVideo");
+      const foundPhoto = services.find(service => service === "Photography");
+      const foundVideo = services.find(service => service === "VideoRecording");
+      if (foundPhoto || foundVideo || foundCombo) {
+        // turn off session
+        services.map(service => {
+          switch (service) {
+            case 'WeddingSession':
+              services = services.filter(service => service !== 'WeddingSession');
+              break;
+          }
+        });
+
+        if ((foundPhoto || foundCombo) && year === 2022) {
+          // freebie
+        } else {
+          services.push('SessionBundle');
         }
-      });
-
-      if ((foundPhoto || foundCombo) && year === 2022) {
-        // freebie
-      } else {
-        services.push('SessionBundle');
       }
     }
 
